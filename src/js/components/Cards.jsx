106285bcfb772afc125e75a13c6a18f311c44605
Andrew Matsumura <42308941+glader0293@users.noreply.github.com>
2025-04-17 00:12:02 +0000

    Final Version

--- src/js/components/Cards.jsx
+++ src/js/components/Cards.jsx
@@ -6,8 +6,8 @@ return(
     <div className="container p-0 card g-2" style={{width: "20rem"}}>
       <img className="card-img-top" src={image} alt="Card image cap"/>
       <div className="card-body m-0">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
+        <h5 className="card-title text-center">{title}</h5>
+        <p className="card-text text-center">{description}</p>
       </div>
       <hr className = "m-0 p-0"></hr>
       <div className ="container d-flex justify-content-center my-2">
